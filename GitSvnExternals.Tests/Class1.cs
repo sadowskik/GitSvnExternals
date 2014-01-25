98b1084c24132fbc1b35decaddc3b54f7c93c190
Krzysztof Sadowski <k.sadowskik@gmail.com>
2014-01-25 15:15:46 +0100

    add project structure

---
+++ GitSvnExternals.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GitSvnExternals.Tests
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void test()
+        {
+            
+        }
+    }
+}
