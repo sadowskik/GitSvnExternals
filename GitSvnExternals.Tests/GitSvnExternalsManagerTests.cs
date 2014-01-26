592b06ae5e34a04c3626c626fe7bbe835d0869cb
Krzysztof Sadowski <k.sadowskik@gmail.com>
2014-01-26 17:00:59 +0100

    separate tests

--- GitSvnExternals.Tests/GitSvnExternalsManagerTests.cs
+++ GitSvnExternals.Tests/GitSvnExternalsManagerTests.cs
@@ -57,32 +57,7 @@ namespace GitSvnExternals.Tests
 
             Check.That(Directory.Exists(TestRepoPath + @"\.git_externals")).IsTrue();
         }
-
-        [Fact]
-        public void clones_external_as_a_separate_repository()
-        {
-            _gitSvnExternalsManager.Clone(new DirectoryExternal(
-                remotePath: new Uri(@"https://subversion.assembla.com/svn/svnandgittest/trunk/Externals"),
-                localPath: ".buildtools"));
-
-            var cloneCommand = _mockedRunner.ExecutedCommands[0];
-
-            Check.That(cloneCommand.Command).IsEqualTo("git");
-            Check.That(cloneCommand.Arguments).IsEqualTo(@"svn clone -r HEAD https://subversion.assembla.com/svn/svnandgittest/trunk/Externals .git_externals\.buildtools");
-        }
-
-        [Fact]
-        public void links_external_to_cloned_dir()
-        {
-            var external = new DirectoryExternal(
-                remotePath: new Uri(@"https://subversion.assembla.com/svn/svnandgittest/trunk/Externals"),
-                localPath: ".buildtools");
-
-            external.Link(TestRepoPath);
-
-            Check.That(Directory.Exists(TestRepoPath + @"\.buildtools")).IsTrue();
-        }
-
+        
         private const string TestRepoPath = @"C:\Projects\testsvngit2";
 
         private const string Externals = @"
