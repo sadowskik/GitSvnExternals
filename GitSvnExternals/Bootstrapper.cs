4c5c737907d36febbb5361f35e1d35f17163e8fe
Krzysztof Sadowski <k.sadowskik@gmail.com>
2014-01-26 18:57:29 +0100

    WPF app skeleton

---
+++ GitSvnExternals/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+using GitSvnExternals.ViewModels;
+
+namespace GitSvnExternals
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Start();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
